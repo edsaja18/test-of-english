4c3284b6412897f3db82e2f5eb54e4de28a1f3b4
Eduardo Salas Jaramillo <149119842+edsaja18@users.noreply.github.com>
2024-04-30 03:36:44 -0600

    Update inicio.php

---
+++ Vistas/inicio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>English Test</title>
+        <link rel="stylesheet" href="lobby.css">
+    </head>
+    <body>
+      
+        
+        <div class="ribbon"></div>
+        
+        <div class="welcome">
+		<div class="content-container">				
+            <div class="content-container d-flex justify-content-center justify-content-lg-end align-items-end h-100 flex-lg-column">
+			<div class="content w-100 w-lg-auto text-right">
+				<h1 class="title">English Test</h1>
+				<p>Pon a prueba tus habilidades de Inglés<br>Inicia Sesión o Registrate para comenzar tu prueba</p>
+                <br>
+				<a href="usuario.html" class="button">Comienza</a>
+				
+		</div>
+	</div>
+</div>
+
+    </body>
+</html>
