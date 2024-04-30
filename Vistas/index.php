867830f1b418b4ab6ea5019b540624f3a23e558d
Eduardo Salas Jaramillo <149119842+edsaja18@users.noreply.github.com>
2024-04-30 03:15:31 -0600

    Update index.php

--- Vistas/index.php
+++ Vistas/index.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
+
 <html>
-<head>
-    <title>Prueba PHP</title>
-</head>
-<body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>English Test</title>
+        <link rel="stylesheet" href="lobby.css">
+    </head>
+    <body>
+      
+        
+        <div class="ribbon"></div>
+        
+        <div class="welcome">
+		<div class="content-container">				
+            <div class="content-container d-flex justify-content-center justify-content-lg-end align-items-end h-100 flex-lg-column">
+			<div class="content w-100 w-lg-auto text-right">
+				<h1 class="title pb-2 mb-md-3 mb-lg-4">English Test</h1>
+				<p>Pon a prueba tus habilidades de Inglés.<br>Inicia Sesión o Registrate para comenzar tu prueba.</p>
+				<a href="usuario.html" class="button">Regístrate o Inicia Sesión</a>
+				
+		</div>
+	</div>
+</div>
 
-<?php
-
-    $mensaje = "¡Hola, mundo desde PHP!";
-    echo "<h1>$mensaje</h1>";
-?>
-
-</body>
+    </body>
 </html>
